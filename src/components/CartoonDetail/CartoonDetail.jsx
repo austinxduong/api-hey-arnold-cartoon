94b956328f30103d16a0fd364ef9b34cbda995f4
austinxduong <axduong2@gmail.com>
2021-08-04 11:48:34 -0700

    adjust detail page, with data of the gif. works

--- src/components/CartoonDetail/CartoonDetail.jsx
+++ src/components/CartoonDetail/CartoonDetail.jsx
@@ -2,18 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // lets add image property detail later, we will come back to this
-const CartoonDetail = ({ name }) => {
+const CartoonDetail = ({ gifLink, _id }) => {
 
   return (
     <>
-      <p>{ name }</p>
+      <p>Actual Link to GIF: { gifLink }</p>
+      <p>Actual ID from API: { _id } </p>
     </>
   );
 };
 
 CartoonDetail.propTypes = { 
   _id: PropTypes.number.isRequired,
-  name: PropTypes.string.isRequired,
+  gifLink: PropTypes.string.isRequired,
 };
 
 export default CartoonDetail;
--- src/services/heyArnoldApi.js
+++ src/services/heyArnoldApi.js
@@ -15,7 +15,7 @@ export const fetchCartoonById = async (_id) => {
   //below route is the API route to grab the specific character, however we will adjust something... lets comment out line 13
   // const res = await fetch('https://hey-arnold-api.herokuapp.com/api/v1/characters/5da237699734fdcb7bef8f51');
   // we replaced it was a template literal instead
-  const res = await fetch(`https://hey-arnold-api.herokuapp.com/api/v1/characters/${_id}`);
+  const res = await fetch(`https://hey-arnold-api.herokuapp.com/api/v1/gifs/${_id}`);
   const json = await res.json();
   console.log('by character ID', json);
 
