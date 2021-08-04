df5df97d53d2a6701da2ca10a22de413c3f34d33
austinxduong <axduong2@gmail.com>
2021-08-03 23:52:32 -0700

    directory structure, + add routes for api

---
+++ src/components/app/App.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable max-len */
+import React, { Component } from 'react';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+
+export default class App extends Component {
+
+  render() {
+    return (
+
+
+      <Router>
+        <Switch>
+
+          <Route path="/" exact={true}
+            component={addComponentVariableHere}
+          />
+          {/* value of the route path below, will be the first key, that lives inside a nested array */}
+          {/* this grabs the specific character, by its _id, as shown in the api json data */}
+          <Route path="/:_id" exact={true}
+            component={addComponentDetailPageHere}
+          />
+
+        </Switch>
+      </Router>
+    );
+  }
+
+}
