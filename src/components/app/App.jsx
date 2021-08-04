1a8ebd598582fcd4615bac246d731763470b8544
austinxduong <axduong2@gmail.com>
2021-08-04 11:31:57 -0700

    hooked all components up, api rendered. DONE

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,8 @@
 /* eslint-disable max-len */
 import React, { Component } from 'react';
+// the fun part, lets hook everything we created into the router
+import Cartoon from '../../containers/Cartoon';
+import CartoonDetailPage from '../../containers/CartoonDetailPage';
 
 import {
   BrowserRouter as Router,
@@ -17,12 +20,12 @@ export default class App extends Component {
         <Switch>
 
           <Route path="/" exact={true}
-            // component={addComponentVariableHere}
+            component={Cartoon}
           />
           {/* value of the route path below, will be the first key, that lives inside a nested array */}
           {/* this grabs the specific character, by its _id, as shown in the api json data */}
           <Route path="/:_id" exact={true}
-            // component={addComponentDetailPageHere}
+            component={CartoonDetailPage}
           />
 
         </Switch>
