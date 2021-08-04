aa11f07bb069995fb1aa0332b03255a935ce9f65
austinxduong <axduong2@gmail.com>
2021-08-04 11:06:36 -0700

    add presentational component, list of cartoons

---
+++ src/components/CartoonList/CartoonList.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Cartoon from '../Cartoon/Cartoon';
+
+const CartoonList = ({ cartoons }) => {
+  const cartoonElements = cartoons.map((cartoon) => (
+    <li key={cartoon._id}>
+      <Cartoon
+        {...cartoon}
+      />
+    </li>
+  ));
+
+  return <ul>{cartoonElements}</ul>;
+};
+
+CartoonList.propTypes = {
+  cartoons: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      image: PropTypes.string.isRequired,
+    })
+  ).isRequired
+};
+
+export default CartoonList;
